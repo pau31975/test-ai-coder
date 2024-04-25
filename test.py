1e54aa41e028cb0e799e7cb5eeab1d4aba1b2c7d
pau31975 <pau31975@gmail.com>
2024-04-25 16:16:09 +0700

    update svc and explain

--- test.py
+++ test.py
@@ -12,6 +12,13 @@ X = iris.data[:, :2]  # we only take the first two features.
 y = iris.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
+#convert dataset to dataframe
+import pandas as pd
+df_data = pd.DataFrame(iris.data)
+df_target = pd.DataFrame(iris.target)
+print(df_data)
+print(df_target)
+
 #generate SVC model
 def test_SVC(X_train, X_test, y_train, y_test):
     clf = svm.SVC(kernel='linear', C=1).fit(X_train, y_train)   
